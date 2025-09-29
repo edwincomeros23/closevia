84268cc3292fda65abb3691ee092d3fe0437c0d2
Sha <exssasha@gmail.com>
2025-09-29 23:28:47 +0800

    hellotest2

--- main.go
+++ main.go
@@ -1,5 +1,4 @@
 package main
-//hellofrancis
 import (
 	"log"
 	"os"
@@ -15,7 +14,7 @@ import (
 )
 
 func main() {
-	// Load environment variables
+	// Load environment variables for francistest connection
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found, using default values")
 	}
