654fd8483326766510a71fdea8addf692bb3a64a
Sha <exssasha@gmail.com>
2025-10-19 18:35:31 +0800

    bug fixes

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -509,52 +509,17 @@ const Dashboard: React.FC = () => {
                  )}
                </Box>
  
-               <Box position="relative">
-               <IconButton
-                   aria-label="Offers"
-                   icon={<Icon as={FaHandshake} />}
-                size="lg"
-              >
-                Add New Product
-              </Button>
-
-              <Box position="relative">
-                <IconButton
-                  aria-label="Notifications"
-                  icon={<BellIcon />}
-                  size="lg"
-                  variant="ghost"
-                  onClick={() => navigate('/notifications')}
-                />
-                {unreadNotifications > 0 && (
-                  <Box
-                    position="absolute"
-                    top="0"
-                    right="0"
-                    transform="translate(30%,-30%)"
-                    bg="red.500"
-                    color="white"
-                    fontSize="xs"
-                    px={2}
-                    py={0.5}
-                    borderRadius="full"
-                  >
-                    {unreadNotifications}
-                  </Box>
-                )}
-              </Box>
-
               <Box position="relative">
                 <IconButton
                   aria-label="Offers"
                   icon={<Icon as={FaHandshake} />}
                   size="lg"
                   variant="ghost"
-                bg="#319795"
-                color="white"
-                _hover={{ bg: '#2A8280' }}
-                _active={{ bg: '#267E7C' }}
-                onClick={() => navigate('/offers')}
+                  bg="#319795"
+                  color="white"
+                  _hover={{ bg: '#2A8280' }}
+                  _active={{ bg: '#267E7C' }}
+                  onClick={() => navigate('/offers')}
                 />
                 {unreadOffers > 0 && (
                   <Badge
@@ -575,11 +540,11 @@ const Dashboard: React.FC = () => {
                     {unreadOffers > 99 ? '99+' : unreadOffers}
                   </Badge>
                 )}
-               </Box>
- 
+              </Box>
+
               <Avatar name={user?.name || 'User'} size="sm" bg="white" />
-             </HStack>
-           </Box>
+            </HStack>
+          </Box>
          </Flex>
  
          {/* Stats */}
@@ -761,83 +726,11 @@ const Dashboard: React.FC = () => {
 
         {/* Popup Modal System */}
         <PopupModal />
-      {/* Notifications are handled on their own page at /notifications */}
+        {/* Notifications are handled on their own page at /notifications */}
       </VStack>
     </Container>
+    </Box>
   )
 }
 
 export default Dashboard
-                        <PaginationControls
-                          currentPage={currentPage}
-                          totalPages={getTotalPages(userProducts)}
-                          onPageChange={setCurrentPage}
-                          itemsCount={userProducts.length}
-                        />
-                      </>
-                    )}
-                   </Box>
- 
-                 </VStack>
-               </TabPanel>
- 
-               {/* Traded/Bartered Items Tab */}
-               <TabPanel>
-                 <VStack spacing={6} align="stretch">
-                   <Box>
-                     <HStack justify="space-between" align="center" mb={4}>
-                       <Heading size="md" color="blue.600">
-                         My Traded/Bartered Items ({tradedItems.length})
-                   </Heading>
-                       <Badge colorScheme="blue" variant="subtle" px={3} py={1}>
-                         Exchange History
-                       </Badge>
-                     </HStack>
-                     
-                     {tradedItems.length === 0 ? (
-                       <Box 
-                         textAlign="center" 
-                         py={12} 
-                         bg="blue.50" 
-                         borderRadius="lg" 
-                         border="1px dashed" 
-                         borderColor="blue.200"
-                       >
-                         <Text color="gray.500" fontSize="lg" mb={2}>
-                           No completed trades yet
-                         </Text>
-                         <Text color="gray.400" fontSize="sm">
-                           Start trading to see your exchange history here!
-                       </Text>
-                     </Box>
-                   ) : (
-                       <>
-                         <SimpleGrid columns={{ base: 1, md: 2, lg: 3, xl: 4 }} spacing={4}>
-                           {getPaginatedItems(tradedItems, tradedCurrentPage).map((product) => (
-                             <ProductCard key={product.id} product={product} showActions={false} />
-                           ))}
-                         </SimpleGrid>
-                         <PaginationControls
-                           currentPage={tradedCurrentPage}
-                           totalPages={getTotalPages(tradedItems)}
-                           onPageChange={setTradedCurrentPage}
-                           itemsCount={tradedItems.length}
-                         />
-                       </>
-                     )}
-                   </Box>
-                 </VStack>
-               </TabPanel>
-             </TabPanels>
-           </Tabs>
-         </Box>
- 
-         {/* Popup Modal System */}
-         <PopupModal />
-       </VStack>
-      </Container>
-    </Box>
-   )
- }
- 
- export default Dashboard
